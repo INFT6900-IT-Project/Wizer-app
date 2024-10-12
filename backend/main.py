2965251ef18841e6602762f35ac41270b47f0fc1
drewhwg <qhg.work@gmail.com>
2024-10-13 02:58:12 +1100

    login function with db connection

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,26 @@
-from fastapi import Depends, FastAPI
 import uvicorn
-# from .dependencies import 
-from routers import user_auth
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+# from .dependencies import
+from routers import user_handling
 
 # app = FastAPI(dependencies=[Depends()])
 app = FastAPI()
 
-app.include_router(user_auth.router)
+app.include_router(user_handling.router)
+
+origins = [
+    "",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # Allows all origins from the list
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="localhost", port=8000, reload=True)
+    uvicorn.run(app, host="localhost", port=3000, reload=True)
