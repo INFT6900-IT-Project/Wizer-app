287f9d95e1dd679fe007d89b7867ca9410a01086
Adoria Duong <bngoc4991@gmail.com>
2024-11-15 00:20:00 +1100

    Adoria Update

--- src/components/Pages/User/UserComponents/UserAchievement.js
+++ src/components/Pages/User/UserComponents/UserAchievement.js
@@ -1,12 +1,75 @@
 import React from 'react';
 import './UserAchievement.css';
+import achievement from '../../../../ImageVideo/achievement.png';
+import Badge from './Badge';
+import badge1 from '../../../../ImageVideo/badge1.jpg';
+import badge2 from '../../../../ImageVideo/badge2.jpg';
+import badge3 from '../../../../ImageVideo/badge3.jpg';
+import badge4 from '../../../../ImageVideo/badge4.jpg';
+import badge5 from '../../../../ImageVideo/badge5.jpg';
+import badge6 from '../../../../ImageVideo/badge6.jpg';
+import badge7 from '../../../../ImageVideo/badge7.jpg';
+import badge8 from '../../../../ImageVideo/badge8.jpg';
+
 
 function UserAchievement() {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <div>
+            <h1 className='ad-user-pages-title'>My Achievements</h1>
+            <div className='ad-achieve-wrap'>
+                <div className='ad-achieve-header'>
+                    <div className='ad-achieve-header-left'>
+                        <p>Where you can track your progress, reflect on the milestones you've reached, and celebrate your hard-earned success. Whether you’re completing courses, acing quizzes, or engaging with the community, each achievement represents a unique accomplishment that brings you closer to mastering new skills.</p>
+                        <p>As you progress, you'll <span>unlock badges</span> that showcase your dedication, consistency, and growth. These badges are not just symbols—they’re proof of your commitment to learning, problem-solving, and personal development. Every achievement is a reflection of your passion for growth and your drive to succeed</p>
+                        <a href='/user-screen/my-learning'>Gain badges</a>
+                    </div>
+                    <div className='ad-achieve-header-right'>
+                        <img src={achievement} />
+                    </div>
+                </div>
+                <div className='ad-achieve-content'>
+                    <h2><i class="fa-solid fa-shield-halved"></i> YOUR BADGES <i class="fa-solid fa-shield-halved"></i></h2>
+                    <div className='ad-badge-container'>
+                        <Badge
+                            src={badge1}
+                            badge_name='First Course Completed'
+                        />
+                        <Badge
+                            src={badge2}
+                            badge_name='Quiz Pro'
+                        />
+                        <Badge
+                            src={badge4}
+                            badge_name='Feedback Champion'
+                        />
+                        <Badge
+                            src={badge3}
+                            badge_name='Rapid Learner'
+                        />
+                        <Badge
+                            src={badge6}
+                            badge_name='Feedback Champion'
+                        />
+                        <Badge
+                            src={badge5}
+                            badge_name='High Achiever'
+                        />
+                        <Badge
+                            src={badge7}
+                            badge_name='Knowledge Sharer'
+                        />
+                        <Badge
+                            src={badge8}
+                            badge_name='Consistency is Key'
+                        />
+
+
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    )
 }
 
 export default UserAchievement
--- src/components/Pages/User/UserComponents/UserSideBar.js
+++ src/components/Pages/User/UserComponents/UserSideBar.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import './UserSideBar.css';
-import { Link } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
+import { useState } from 'react';
 
 function UserSideBar() {
+
+
   return (
     <div>
       <div className='ad-user-sidebar-wrap'>
-      <Link to ='/user-screen/achievement' style={{textDecoration:'none'}}><p><i class="fa-solid fa-trophy"></i> Achievements</p></Link>
-      <Link to ='/user-screen/reminder' style={{textDecoration:'none'}}><p><i class="fa-solid fa-bell"></i> Reminder</p></Link>
-      <Link to ='/user-screen/saved-courses' style={{textDecoration:'none'}}><p><i class="fa-solid fa-bookmark"></i> Saved Courses</p></Link>
-      <Link to ='/user-screen/progress' style={{textDecoration:'none'}}><p><i class="fa-solid fa-chart-line"></i> Progress</p></Link>
-      <Link to ='/user-screen/help-center' style={{textDecoration:'none'}}><p><i class="fa-regular fa-circle-question"></i> Help Center</p></Link>
-      <Link to ='/user-screen/account-settings' style={{textDecoration:'none'}}><p><i class="fa-solid fa-gear"></i> Account Settings</p></Link>
+        <Link to='/user-screen/achievement' style={{ textDecoration: 'none' }}><p><i class="fa-solid fa-trophy"></i> Achievements</p></Link>
+        <Link to='/user-screen/reminder' style={{ textDecoration: 'none' }}><p><i class="fa-solid fa-bell"></i> Reminder</p></Link>
+        <Link to='/user-screen/saved-courses' style={{ textDecoration: 'none' }}><p><i class="fa-solid fa-bookmark"></i> Saved Courses</p></Link>
+        <Link to='/user-screen/progress' style={{ textDecoration: 'none' }}><p><i class="fa-solid fa-chart-line"></i> Progress</p></Link>
+        <Link to='/user-screen/help-center' style={{ textDecoration: 'none' }}><p><i class="fa-regular fa-circle-question"></i> Help Center</p></Link>
+        <Link to='/user-screen/account-settings' style={{ textDecoration: 'none' }}><p><i class="fa-solid fa-gear"></i> Account Settings</p></Link>
       </div>
     </div>
   )
