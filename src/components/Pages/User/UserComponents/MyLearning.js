96c1cb109e4a597448a703949dffd82181c7a2c4
Adoria Duong <bngoc4991@gmail.com>
2024-11-15 00:44:08 +1100

    Adoria Update User Screen

--- src/components/Pages/User/UserComponents/MyLearning.js
+++ src/components/Pages/User/UserComponents/MyLearning.js
@@ -1,8 +1,12 @@
-import React from 'react'
+import React from 'react';
+import './MyLearning.css'
 
 function UserDashboard() {
   return (
     <div>
+      <div className='ad-my-learning-wrap'>
+        <h1>hi</h1>
+      </div>
       
     </div>
   )
